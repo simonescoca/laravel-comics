d588ea594fcbfee705233e301242cba0cdbb6e78
simonescoca <simone.scoca@icloud.com>
2023-07-19 19:28:59 +0200

    added array to return in app.blade.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $comics = config ('comics.comics');
+    return view ('layouts.app', compact ('comics'));
+}) -> name ('layouts.app');
